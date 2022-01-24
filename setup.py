9ec5e3769a4248837c8e4c90351ed58f8449d998
Matthew Marshall <mwrshl@gmail.com>
2022-01-23 23:37:18 -0600

    add requirements.txt

--- setup.py
+++ setup.py
@@ -2,15 +2,5 @@ from setuptools import setup
 
 setup(name="memorize",
       license="MIT",
-      packages=["memorize"],
-      install_requires=[
-          'contractions',
-          'vosk',
-          'nltk',
-          'cattrs',
-          'pendulum',
-          'click',
-          'fuzzy',
-          'metaphone',
-          'fuzzywuzzy',
-      ])
+      packages=["memorize"]
+      )
